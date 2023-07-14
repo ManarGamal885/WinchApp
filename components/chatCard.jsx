bd2b3af22bb10544706c36d49f307c4406642579
ManarGamal885 <manarg281@gmail.com>
2023-07-14 17:56:39 +0300

    adding the chatList

--- components/chatCard.jsx
+++ components/chatCard.jsx
@@ -14,19 +14,70 @@ import React, {
     useState
 } from "react";
 import Colors from "../colors"
+import p from '../assets/others/p.jpg'
 export default function chatCard() {
+    const [img, setImg] = useState("");
+    const [name, setname] = useState("Fatima Ahmed");
+    const [timeLastmsg, settimeLastmsg] = useState();
     return (
-        <View style={styles.cont}>
-            <Text>hi</Text>
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <View style={styles.cont}>
+                    <Image source={p} style={styles.person}/>
+                    <Text style={styles.nameStyle}>{name}</Text>
+                    <Text style={styles.timeStyle}>2:00 AM</Text>
+                </View>
+            </TouchableOpacity>
+           
         </View>
     )
 }
 const styles = StyleSheet.create({
-    cont:{
-        width:'90%',
-        height:'10%',
-        backgroundColor:Colors.buttons,
-        marginTop:20
+    container: {
+        width: '95%',
+        height: '13%',
+        overflow: 'hidden',
+        paddingBottom: 10,
+        paddingLeft: 0.5,
+        paddingRight: 0.5,
+        paddingTop: 0.09,
+        alignSelf: 'center',
+        marginTop: 20
     },
-    
+    cont: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: "#fff",
+        alignSelf: 'center',
+        borderRadius: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.4,
+        shadowRadius: 2,
+        elevation: 5,
+        flexDirection:'row'
+    },
+    person:{
+        width:76.59,
+        height:73.32,
+        borderRadius:50,
+        marginLeft:30,
+        alignSelf:'center',
+    },
+    nameStyle:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        fontFamily: Colors.fontFamily,
+        color: Colors.font,
+        alignSelf:'center',
+        marginHorizontal:'10%'
+    },
+    timeStyle:{
+        fontSize: 13,
+        fontFamily: Colors.fontFamily,
+        color: '#000',
+        alignSelf:'center',
+        opacity:0.5
+    }
+
 })
--- pages/user/MessagesList.jsx
+++ pages/user/MessagesList.jsx
@@ -15,21 +15,70 @@ import React, {
 } from "react";
 import Colors from "../../colors"
 import ChatCard from "../../components/chatCard"
+import arrow from '../../assets/gen/Arrow3(1).png'
+
 export default function MessagesList() {
-  return (
-    <View  style={styles.cont}>
-        <ChatCard />
-    </View>
-  )
+    return (
+        <View style={styles.cont}>
+            <View style={styles.headerCont}>
+
+                <View style={styles.arrowCont}>
+                    <TouchableOpacity >
+                        <Image source={arrow} style={styles.arrow} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.chatHeaderCont}>
+                    <Text style={styles.chatHeader}>Chat</Text>
+                </View>
+                
+            </View>
+            <ChatCard />
+        </View>
+    )
 }
 const styles = StyleSheet.create({
-    cont:{
-        width:'100%',
-        height:'100%',
-        flex:1,
-        flexDirection:'row',
-        justifyContent:'center'
+    cont: {
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: Colors.base
     },
-    
-
+    headerCont: {
+        width: '100%',
+        height: '8%',
+        backgroundColor: Colors.base,
+        borderBottomWidth: 1,
+        borderBottomColor: '#D9D9D9',
+        flexDirection: 'row',
+        alignSelf:'flex-end',
+        marginBottom:15
+    },
+    chatHeaderCont: {
+        width: '82%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+    arrowCont: {
+        width: '9%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+    },
+    chatHeader: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        fontFamily: Colors.fontFamily,
+        color: Colors.buttons,
+        elevation:2,
+    },
+    arrow: {
+        width: 8,
+        height: 15,
+        alignSelf: 'flex-start',
+        marginTop: 5,
+    },
 })
